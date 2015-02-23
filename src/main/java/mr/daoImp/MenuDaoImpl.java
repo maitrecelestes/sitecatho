df45c3c2fa2b4bdb28f13e3e4cd13b220c300385
unknown <michel.guignier@hei.fr>
2015-02-23 17:29:55 +0100

    Creation de fonction et test pour la fonction menu

--- src/main/java/mr/daoImp/MenuDaoImpl.java
+++ src/main/java/mr/daoImp/MenuDaoImpl.java
@@ -13,16 +13,25 @@ import mr.entities.Menu;
 
 public class MenuDaoImpl implements MenuDao {
 
+	//Afficher le Menu
 	@Override
 	public List<Menu> listerMenu() {
 		List<Menu> maListMenu = new ArrayList<Menu>();
 		try {
-			Connection connection = DataSourceProvider.getDataSource().getConnection();
+			Connection connection = DataSourceProvider.getDataSource()
+					.getConnection();
 			Statement stmt = connection.createStatement();
 
-			ResultSet rs = stmt.executeQuery("SELECT idpage, nompage, rang FROM page ORDER BY idpage");
+			ResultSet rs = stmt
+					.executeQuery("SELECT idpage, nompage, rang FROM page ORDER BY idpage");
 			while (rs.next()) {
-				maListMenu.add(new Menu(rs.getInt("idpage"), rs.getString("nompage"), rs.getInt("rang")/*, rs.getBoolean("visibilite")*/));
+				maListMenu.add(new Menu(rs.getInt("idpage"), rs
+						.getString("nompage"), rs.getInt("rang")/*
+																 * ,
+																 * rs.getBoolean
+																 * (
+																 * "visibilite")
+																 */));
 			}
 			connection.close();
 		} catch (SQLException e) {
@@ -32,10 +41,122 @@ public class MenuDaoImpl implements MenuDao {
 	}
 
 	@Override
+	public List<Menu> listerMenuDeRang0() {
+		List<Menu> maListMenuRang0 = new ArrayList<Menu>();
+		try {
+			Connection connection = DataSourceProvider.getDataSource()
+					.getConnection();
+			Statement stmt = connection.createStatement();
+
+			ResultSet rs = stmt
+					.executeQuery("SELECT idpage, nompage, rang FROM page WHERE rang = 0 ORDER BY idpage");
+			while (rs.next()) {
+				maListMenuRang0.add(new Menu(rs.getInt("idpage"), rs
+						.getString("nompage"), rs.getInt("rang")/*
+																 * ,
+																 * rs.getBoolean
+																 * (
+																 * "visibilite")
+																 */));
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return maListMenuRang0;
+
+	}
+	
+	@Override
+	public List<Menu> listerMenuDeRang1Entre2Rang0(int premierRang0,
+			int secondRang0) {
+		List<Menu> maListMenuRang1Entre2Rang0 = new ArrayList<Menu>();
+		try {
+			Connection connection = DataSourceProvider.getDataSource()
+					.getConnection();
+			Statement stmt = connection.createStatement();
+
+			ResultSet rs = stmt
+					.executeQuery("SELECT idpage, nompage, rang FROM PAGE WHERE rang =0 AND idpage>= "
+							+ premierRang0
+							+ " AND idpage<= "
+							+ secondRang0
+							+ " ORDER BY idpage");
+			while (rs.next()) {
+				maListMenuRang1Entre2Rang0.add(new Menu(rs.getInt("idpage"), rs
+						.getString("nompage"), rs.getInt("rang")/*
+																 * ,
+																 * rs.getBoolean
+																 * (
+																 * "visibilite")
+																 */));
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return maListMenuRang1Entre2Rang0;
+	}
+	
+	//Fonction generale
+	@Override
+	public void supprimerBaseDeDonneesMenu() {
+		try {
+			Connection connection = DataSourceProvider.getDataSource()
+					.getConnection();
+			PreparedStatement stmt = connection.prepareStatement(
+					"DELETE FROM page",
+					Statement.RETURN_GENERATED_KEYS);
+			stmt.executeUpdate();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Override
+	public void regenererBaseDeDonneesMenu(List<Menu> maNouvelleListe) {
+		supprimerBaseDeDonneesMenu();
+		for(int i=0;i<maNouvelleListe.size();i++){
+			ajouterMenu(maNouvelleListe.get(i));
+		}
+		
+	}
+	
+	//Ajouter un nouveau menu : besoin id precedent et nouveau menu
+	@Override
+	public void ajouterNouveauMenu(int idLigneprecedente, Menu nouveauMenu) {
+		List<Menu> maListe = listeChangerOrdreNouveauId(idLigneprecedente,nouveauMenu);
+		regenererBaseDeDonneesMenu(maListe);
+	}
+	
+	//Fonction pour ajouter nouveau Menu
+	@Override
+	public List<Menu>  listeChangerOrdreNouveauId(int idLigneprecedente,
+			Menu nouveauMenu) {
+		List<Menu> maListe = listerMenu();
+		List<Menu> maNewListe = new ArrayList<Menu>();
+		for(int i=0; i <= idLigneprecedente ; i++){
+			maNewListe.add(maListe.get(i));
+		}
+		nouveauMenu.setIdpage(idLigneprecedente+1);
+		maNewListe.add(nouveauMenu);
+		for(int i=idLigneprecedente+1; i < maListe.size() ; i++){
+			maListe.get(i).setIdpage(i+1);
+			maNewListe.add(maListe.get(i));
+		}
+		return maNewListe;		
+	}
+	
+	@Override
 	public void ajouterMenu(Menu newMenu) {
 		try {
-			Connection connection = DataSourceProvider.getDataSource().getConnection();
-			PreparedStatement stmt = connection.prepareStatement("INSERT INTO page(idpage, nompage, rang) VALUES(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			Connection connection = DataSourceProvider.getDataSource()
+					.getConnection();
+			PreparedStatement stmt = connection.prepareStatement(
+					"INSERT INTO page(idpage, nompage, rang) VALUES(?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
 			stmt.setInt(1, newMenu.getIdpage());
 			stmt.setString(2, newMenu.getNompage());
 			stmt.setInt(3, newMenu.getRang());
@@ -45,78 +166,41 @@ public class MenuDaoImpl implements MenuDao {
 			e.printStackTrace();
 		}
 	}
-
+		
+	//Fonction pour supprimer un menu
 	@Override
 	public void supprimerMenu(int numeroArticle) {
 		Connection connection;
 		try {
 			connection = DataSourceProvider.getDataSource().getConnection();
-			PreparedStatement stmt= connection.prepareStatement("DELETE FROM `page` WHERE idpage =?");
-			stmt.setInt(1,numeroArticle); 
+			PreparedStatement stmt = connection
+					.prepareStatement("DELETE FROM `page` WHERE idpage =?");
+			stmt.setInt(1, numeroArticle);
 			stmt.executeUpdate();
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
+	}
+//////////////////////////////////////////////////////////////////////
+	@Override
+	public void supprimerLigneDansMenu(int idLignesupprimee) {
+		List<Menu> maListe = listerMenu();
+		List<Menu> maNewListe = new ArrayList<Menu>();
 		
+		for(int i=0;i<idLignesupprimee;i++){
+			maNewListe.add(maListe.get(i));
+		}
+		for(int j=idLignesupprimee+1;j<maListe.size();j++){
+			Menu mapage = maListe.get(j);
+			int nb = mapage.getIdpage();
+			mapage.setIdpage(nb-1);
+			maNewListe.add(mapage);
+		}
+		regenererBaseDeDonneesMenu(maNewListe);
 	}
 	
-	
-	@Override
-	public List<Menu> listerMenuDeRang0() {
-		List<Menu> maListMenuRang0 = new ArrayList<Menu>();
-		try {
-			Connection connection = DataSourceProvider.getDataSource().getConnection();
-			Statement stmt = connection.createStatement();
 
-			ResultSet rs = stmt.executeQuery("SELECT idpage, nompage, rang FROM page WHERE rang = 0 ORDER BY idpage");
-			while (rs.next()) {
-				maListMenuRang0.add(new Menu(rs.getInt("idpage"), rs.getString("nompage"), rs.getInt("rang")/*, rs.getBoolean("visibilite")*/));
-			}
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return maListMenuRang0;
-		
-	}
-
-	@Override
-	public List<Menu> listerMenuDeRang1Entre2Rang0(int premierRang0,
-			int secondRang0) {
-		List<Menu> maListMenuRang1Entre2Rang0 = new ArrayList<Menu>();
-		try {
-			Connection connection = DataSourceProvider.getDataSource().getConnection();
-			Statement stmt = connection.createStatement();
-
-			ResultSet rs = stmt.executeQuery("SELECT idpage, nompage, rang FROM PAGE WHERE rang =0 AND idpage>= "+premierRang0+" AND idpage<= "+secondRang0+" ORDER BY idpage");
-			while (rs.next()) {
-				maListMenuRang1Entre2Rang0.add(new Menu(rs.getInt("idpage"), rs.getString("nompage"), rs.getInt("rang")/*, rs.getBoolean("visibilite")*/));
-			}
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return maListMenuRang1Entre2Rang0;
-	}
-
-	@Override
-	public int chercherSuivantRang0(int idpremierRang0) {
-		List<Menu> maListMenu = listerMenu();
-		int nombreDeMenu = maListMenu.size();
-		
-		if(maListMenu.get(idpremierRang0).getRang()==0 && idpremierRang0+1 != nombreDeMenu){
-			
-			
-			int i=0;
-			while(i<nombreDeMenu && maListMenu.get(i+1).getRang()==1){
-				i=i+1;
-			}
-				return idpremierRang0+i+1;			
-		}else{
-			return -1;
-		}
-		
-	}
 
 }
